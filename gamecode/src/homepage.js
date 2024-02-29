7d3ca1f8ad188e7277db6cd450925c59dfc49633
harshit <harshitshrivastava340@gmail.com>
2024-02-29 05:55:20 +0530

    harshit

---
+++ gamecode/src/homepage.js
@@ -0,0 +1,15 @@
+
+const registerbtn = document.getElementById("register");
+
+
+registerbtn.addEventListener('click', () => {
+    
+    window.location.href = "/register";
+});
+
+
+const loginbtn = document.getElementById("login");
+
+loginbtn.addEventListener('click',()=>{
+  window.location.href="/register";
+})
--- gamecode/src/index.js
+++ gamecode/src/index.js
@@ -8,15 +8,39 @@ const staticpath = path.join(__dirname,'../public');
 const template = path.join(__dirname,'../template/views');
 const partials = path.join(__dirname,'../template/partials');
 
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+
+
+
 app.set('view engine','hbs');
 app.set('views',template);
 hbs.registerPartials(partials);
 
-app.use(express.static(staticpath));
+app.use(express.static(staticpath, {
+    setHeaders: (res, filepath) => {
+        if (filepath.endsWith('.js')) {
+            res.setHeader('Content-Type', 'application/javascript');
+        }
+    }
+}));
 
 app.get("/",(req,res)=>{
-    res.render("progress");
+    res.render("homepage");
 })
+
+
+app.get("/register",(req,res)=>{
+        res.render("registration");
+    })
+
+
+
+
+
+
+
+
 
 app.listen(8000,()=>{
     console.log("hello i am at 8000");
